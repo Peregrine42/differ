6454f670f978655d4e7f8da5e982530bc64fef98
Duncan Fermor (TNK6) <duncan.fermor@bt.com>
2017-10-23 11:44:17 +0100

    validation to only allow json

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,9 @@
 class Page < ApplicationRecord
+  validate :content_contains_valid_characters
+
+  def content_contains_valid_characters
+    unless content.match(/^[A-Za-z0-9_:", {}]+$/)
+      errors.add(:content, 'Content may only contain: [A-Za-z0-9_:", {}]')
+    end
+  end
 end
